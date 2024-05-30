7f75fa863cd257aebd1b89c2704e1836654230ee
bbluloops <nehisbell.terrones12@gmail.com>
2024-05-30 17:15:49 -0500

    estudiante insert

--- services/estudiante.py
+++ services/estudiante.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from model.estudiante import tbEstudiante
+from utils.db import db 
 
 estudiantes=Blueprint('estudiantes', __name__)
 
@@ -18,3 +19,24 @@ def getEstudiantes():
     result["msg"]="Se recupero los contactos sin incovenientes"
     return jsonify(result), 200
 
+@estudiantes.route('/Estudiantes/v1/insert',methods=['POST'])
+def insert():
+    result={}
+    body=request.get_json()
+    nombreEstudiante=body.get('nombreEstudiante')
+    apellidoEstudiante=body.get('apellidoEstudiante')
+    correoEstudiante=body.get('correoEstudiante')
+    contraseñaEstudiante=body.get('contraseñaEstudiante')
+
+    if not nombreEstudiante or not apellidoEstudiante or not correoEstudiante or not contraseñaEstudiante:
+        result["status_code"]=400
+        result["msg"]="Faltan datos"
+        return jsonify(result),400
+    
+    Estudiante=tbEstudiante(nombreEstudiante, apellidoEstudiante, correoEstudiante, contraseñaEstudiante)
+    db.session.add(Estudiante)
+    db.session.commit()
+    result["data"]=Estudiante
+    result["status_code"]=201
+    result["msg"]="Se agrego el estudiante"
+    return jsonify(result),201
