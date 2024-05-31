46753a431951263079898f96715e83ea94c70ce3
BetcyMilagros <betcy.atoccsa@unmsm.edu.pe>
2024-05-31 16:33:58 -0500

    Tabla test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from utils.db import db
 from services.estudiante import estudiantes
+from services.Test import Tests
 from config import DATABASE_CONNECTION
 
 app=Flask(__name__)
@@ -9,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_CONNECTION
 db.init_app(app)
 
 app.register_blueprint(estudiantes)
+app.register_blueprint(Tests)
 
 with app.app_context():
     # Crea todas las tablas definidas en los modelos
---
+++ services/Test.py
@@ -0,0 +1,97 @@
+from flask import Blueprint, request, jsonify
+from model.Test import tbTest
+from utils.db import db
+#DEFINIR RUTAS
+Tests = Blueprint('Tests',__name__)
+
+@Tests.route('/Tests/v1',methods=['GET'])
+def getMensaje():
+    result={}
+    result["data"]='sisvitag3'
+    return jsonify(result)
+
+@Tests.route('/Tests/v1/listar',methods=['GET'])
+def getTests():
+    result={}
+    Tests=tbTest.query.all()
+    result["data"]=Tests
+    result["status_code"]=200
+    result["msg"]="Se recupero los tests sin inconvenientes"
+    return jsonify(result),200
+
+@Tests.route('/Tests/v1/insert', methods=['POST'])
+def insert():
+    result = {}
+    body = request.get_json()
+    autorTest = body.get('autorTest')
+    descripcionTest = body.get('descripcionTest')
+    nombreTest = body.get('nombreTest')
+
+    if not autorTest or not descripcionTest or not nombreTest:
+        result["status_code"] = 400
+        result["msg"] = "Faltan datos"
+        return jsonify(result), 400
+
+    Test = tbTest(autorTest, descripcionTest, nombreTest)
+    db.session.add(Test)
+    db.session.commit()
+    result["data"] = Test
+    result["status_code"] = 201
+    result["msg"] = "Se agrego el test"
+    return jsonify(result), 201
+
+
+@Tests.route('/Tests/v1/update', methods=['POST'])  
+def update():
+    result = {}
+    body = request.get_json()
+    idTest = body.get('idTest')
+    autorTest = body.get('autorTest')
+    descripcionTest = body.get('descripcionTest')
+    nombreTest = body.get('nombreTest')
+
+    if not idTest or not autorTest or not descripcionTest or not nombreTest:
+        result["status_code"] = 400
+        result["msg"] = "Faltan datos"
+        return jsonify(result), 400  
+
+    Test = tbTest.query.get(idTest)
+    if not Test:
+        result["status_code"] = 400
+        result["msg"] = "test no existe"
+        return jsonify(result), 400
+
+    Test.idTest = idTest
+    Test.autorTest = autorTest
+    Test.descripcionTest = descripcionTest
+    Test.nombreTest = nombreTest
+    db.session.commit()
+
+    result["data"] = Test
+    result["status_code"] = 202
+    result["msg"] = "Se modificó el test"
+    return jsonify(result), 202
+
+@Tests.route('/Tests/v1/delete', methods=['DELETE'])
+def delete():
+    result={}
+    body=request.get_json()
+    idTest=body.get('idTest')
+    if not idTest:
+        result["status_code"]=400
+        result["msg"]="Debe consignar un id de test"
+        return jsonify(result),400
+
+    Test=tbTest.query.get(idTest)
+    if not Test:
+        result["status_code"]=400
+        result["msg"]="El test no existe"
+        return jsonify(result),400
+    
+    db.session.delete(Test)
+    db.session.commit()
+
+    result["data"]=Test
+    result["status_code"]=200
+    result["msg"]="Se eliminó el test"
+    return jsonify(result),200
