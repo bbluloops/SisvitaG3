39a8cbb1e7fdb760de66ed1377be23f9ca21a021
DairFernandez28 <dairfernandez28@gmail.com>
2024-06-12 19:37:55 -0500

    Tabla EstadoCita

---
+++ model/estadoCita.py
@@ -0,0 +1,17 @@
+from utils.db import db
+from dataclasses import dataclass
+
+@dataclass
+class tbEstadoCita(db.Model):
+    __tablename__ = 'tbEstadoCita'
+    idEstadoCita: int 
+    idCita: int
+    estadoCita: str
+
+    idEstadoCita = db.Column(db.Integer, primary_key=True)
+    idCita = db.Column(db.Integer, db.ForeignKey('cita.idCita'))
+    estadoCita = db.Column(db.String(30))
+
+    def __init__(self, idCita, estadoCita):
+        self.idCita = idCita
+        self.estadoCita = estadoCita
